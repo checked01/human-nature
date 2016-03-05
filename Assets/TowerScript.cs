1e407661f53deb0a23dbb2d8871749d4f8e6104a
EdgeworthCatfish <CosmicDarkrai@gmail.com>
2016-03-05 13:47:07 -0500

    first
    aaa

---
+++ Assets/TowerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerScript : MonoBehaviour {
+	public GameObject targeter;
+	public int radius;
+	public int rateOfFire;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
